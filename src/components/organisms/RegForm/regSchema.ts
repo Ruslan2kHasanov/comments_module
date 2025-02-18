e18cb10715be3446abc3244ea7cb59c05966dde1
Ruslan kHasanov <khazaruscsu@gmail.com>
2025-02-18 16:38:49 +0500

    Исправлена валидация для регистрации

--- src/components/organisms/RegForm/regSchema.ts
+++ src/components/organisms/RegForm/regSchema.ts
@@ -1,7 +1,14 @@
 import * as yup from 'yup';
 
 export const regSchema = yup.object().shape({
-  password: yup.string().required('Поле обязательно для ввода').trim(),
+  password: yup
+    .string()
+    .required('Поле обязательно для ввода')
+    .trim()
+    .min(8, 'Пароль должен быть не менее 8 символов')
+    .matches(/[A-Za-zА-Яа-я]/, 'Пароль должен содержать буквы')
+    .matches(/[0-9]/, 'Пароль должен содержать цифры')
+    .matches(/[!@#$%^&*(),.?":{}|<>_\-+=]/, 'Пароль должен содержать спец символ'),
   name: yup.string().required('Поле обязательно для ввода').trim(),
   re_password: yup
     .string()
